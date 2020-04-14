4618ee3d120bb97d4a6fa5317a0b10132d883a51
ARYAN RAJ <53479608+aryan6098@users.noreply.github.com>
2020-04-14 08:20:32 +0530

    add a controller and a router for a home

---
+++ controller/home_controller.js
@@ -0,0 +1,9 @@
+module.exports.home = function(req, res)
+{
+    return res.end('<h1>Express is ip for Cod</h1>')
+}
+
+module.exports.actionName = function(req,res)
+{
+    return res.end('<h1>hello</h1>')
+}
